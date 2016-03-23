fb7b75ac148a8f124685da3279cf1f381e43641c
Manuel Strehl <git@manuel-strehl.de>
2016-03-24 00:47:12 +0100

    add Materialize for the look + about page

--- src/main.js
+++ src/main.js
@@ -59,6 +59,10 @@ render.object = function(data, tree) {
     render_item(data[key], item);
     subtree.appendChild(item);
   }
+  if (! subtree.children.length) {
+    subtree.className += ' json-object--empty';
+    subtree.innerHTML = '<dd class="json-object-empty_marker">empty map</dd>';
+  }
   tree.appendChild(subtree);
 }
 
@@ -70,6 +74,10 @@ render.array = function(data, tree) {
       return _li;
     })
   );
+  if (! subtree.children.length) {
+    subtree.className += ' json-array--empty';
+    subtree.innerHTML = '<li class="json-array-item json-array-item--dummy">empty list</li>';
+  }
   tree.appendChild(subtree);
 }
 
@@ -88,45 +96,52 @@ function transform_key(str) {
 
 function parse_string(str) {
   var frag = span('.p-string');
-  frag.innerHTML = str.replace(new RegExp(
-        /* url */
-        '(https?:\\/\\/[a-zA-Z0-9#%&()*+,\\-.\\/:;=?@_~]+)|'+
-        /* email */
-        '([a-zA-Z0-9._\\-+:]+@[a-zA-Z0-9.\\-]+)|'+
-        /* date */
-        '([0-9]{4}-[0-9]{2}-[0-9]{2}(?:[T ][012][0-9]:[0-5][0-9](?::[0-5][0-9])?(?: [A-Z]+| [+}-][0-9:]+)?)?)|'+
-        /* .NET date */
-        '("\\\\\\/Date\\([0-9]+\\)\\\\\\/")|'+
-        /* CSS color */
-        '('+css_colors.join('|')+'|(?:rgb|hsl)a?\\([^)]+\\)|\\b#[a-fA-F0-9]{3}(?:[a-fA-F0-9]{3})?\\b)|'+
-        /* Twitter handle */
-        '(\\b@[a-z_0-9]{1,16})', 'g'),
-    function (m, url, email, date, dNET_date, color, twitter) {
-      if (url) {
-        return '<a class="p-url" href="'+url+'">'+url.replace(/^https?:\/\//, '')+'</a>';
-      }
-      if (email) {
-        return '<a class="p-email" href="mailto:'+email.replace('mailto:', '')+'">'+email.replace('mailto:', '')+'</a>';
-      }
-      if (date) {
-        return '<abbr class="p-date" title="'+date+'">'+(new Date(date.replace(/ /, ''))).toLocaleString()+'</abbr>';
-      }
-      if (dNET_date) {
-        return '<abbr class="p-date p-date--dotnet" title="'+dNET_date+'">'+(new Date(parseInt(dNET_date.replace(/"\\\/Date\(([0-9]+)\)\\\/"/, '$1'), 10))).toLocaleString()+'</abbr>';
-      }
-      if (color) {
-        return '<span class="p-color" style="box-shadow: inset 0 0 4px '+color+'">'+color+'</span>';
-      }
-      if (twitter) {
-        return '<a class="p-twittername" href="https://twitter.com/'+twitter.replace(/^@/, '')+'">'+twitter+'</a>';
-      }
-      return m;
-    });
+  if (str) {
+    frag.innerHTML = str.replace(new RegExp(
+          /* url */
+          '(https?:\\/\\/[a-zA-Z0-9#%&()*+,\\-.\\/:;=?@_~]+)|'+
+          /* email */
+          '([a-zA-Z0-9._\\-+:]+@[a-zA-Z0-9.\\-]+)|'+
+          /* date */
+          '([0-9]{4}-[0-9]{2}-[0-9]{2}(?:[T ][012][0-9]:[0-5][0-9](?::[0-5][0-9])?(?: [A-Z]+| [+}-][0-9:]+)?)?)|'+
+          /* .NET date */
+          '("\\\\\\/Date\\([0-9]+\\)\\\\\\/")|'+
+          /* CSS color */
+          '('+css_colors.join('|')+'|(?:rgb|hsl)a?\\([^)]+\\)|#[a-fA-F0-9]{3}(?:[a-fA-F0-9]{3})?\\b)|'+
+          /* Twitter handle */
+          '(\\b@[a-z_0-9]{1,16})', 'g'),
+      function (m, url, email, date, dNET_date, color, twitter) {
+        if (url) {
+          return '<a class="p-url" href="'+url+'">'+url.replace(/^https?:\/\//, '')+'</a>';
+        }
+        if (email) {
+          return '<a class="p-email" href="mailto:'+email.replace('mailto:', '')+'">'+email.replace('mailto:', '')+'</a>';
+        }
+        if (date) {
+          return '<abbr class="p-date" title="'+date+'">'+(new Date(date.replace(/ /, ''))).toLocaleString()+'</abbr>';
+        }
+        if (dNET_date) {
+          return '<abbr class="p-date p-date--dotnet" title="'+dNET_date+'">'+(new Date(parseInt(dNET_date.replace(/"\\\/Date\(([0-9]+)\)\\\/"/, '$1'), 10))).toLocaleString()+'</abbr>';
+        }
+        if (color) {
+          return '<span class="p-color" style="box-shadow: inset 0 0 4px '+color+'">'+color+'</span>';
+        }
+        if (twitter) {
+          return '<a class="p-twittername" href="https://twitter.com/'+twitter.replace(/^@/, '')+'">'+twitter+'</a>';
+        }
+        return m;
+      });
+    // TODO: if the string contains HTML, it should be escaped
+  } else {
+    frag.className += ' p-string--empty';
+    frag.textContent = '(empty)';
+  }
   return frag;
 }
 
 render.number = function(data, tree) {
-  tree.appendChild(span('.json-number', data.toString()));
+  var method = data.toLocaleString? 'toLocaleString' : 'toString';
+  tree.appendChild(span('.json-number', data[method]()));
 }
 
 render.boolean = function(data, tree) {
@@ -138,7 +153,9 @@ render.boolean = function(data, tree) {
 }
 
 render.null = function(data, tree) {
-  tree.appendChild(span('.json-null', '\u2014'));
+  tree.appendChild(span('.json-null', {
+    title: 'null',
+  }, '\u2014'));
 }
 
 document.getElementById('render').addEventListener('click', render_view);
